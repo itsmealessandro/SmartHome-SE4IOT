98bd951b0c7bba9db3efa0277f8af46b2f16cf7e
itsmealessandro <itsmealessandro@outlook.com>
2025-01-27 16:52:50 +0100

    [sensors] 2 rooms temperature

--- sensors/sensorsSetup.sh
+++ sensors/sensorsSetup.sh
@@ -7,19 +7,24 @@ cd ./sens/
 
 echo -e "\e[1;33m Activating test sensor \e[0m"
 echo "---------------------------------------------------------------------"
-java -cp ./org.eclipse.paho.client.mqttv3-1.2.0.jar ./Sensor1.java &
+java -cp ./org.eclipse.paho.client.mqttv3-1.2.0.jar ./MyclientSample.java &
 PID1=$!
 
 echo -e "\e[1;33m Activating sensor1 \e[0m"
 echo "---------------------------------------------------------------------"
-java -cp ./org.eclipse.paho.client.mqttv3-1.2.0.jar ./MyclientSample.java &
+java -cp ./org.eclipse.paho.client.mqttv3-1.2.0.jar ./Sensor1.java &
 PID2=$!
+
+echo -e "\e[1;33m Activating sensor2 \e[0m"
+echo "---------------------------------------------------------------------"
+java -cp ./org.eclipse.paho.client.mqttv3-1.2.0.jar ./Sensor2.java &
+PID3=$!
 
 echo "---------------------------------------------------------------------"
 echo -e "\e[1;33m All sensors activated \e[0m"
 
 # WARNING: Begore shutting down this
 # script and the related container we have to wait that both clients have finished
-wait $PID1 $PID2
+wait $PID1 $PID2 $PID3
 
 echo -e "\e[1;31m Sensors container script is over \e[0m"
