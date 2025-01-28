d0f506bf9e9da08f2039916b47ec023f13f7081f
itsmealessandro <itsmealessandro@outlook.com>
2025-01-28 19:20:51 +0100

    [sensors-nodered-alarm] fixed stuff

--- sensors/sens/MyclientSample.java
+++ sensors/sens/MyclientSample.java
@@ -20,41 +20,41 @@ public class MqttPublishSample {
     MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
     MqttConnectOptions connOpts = new MqttConnectOptions();
     connOpts.setCleanSession(true);
-    System.out.println("------------------------------------------------------------");
-    System.out.println("Connecting to broker: " + broker);
+//     System.out.println("------------------------------------------------------------");
+//     System.out.println("Connecting to broker: " + broker);
     sampleClient.connect(connOpts);
-    System.out.println("Connected");
-    System.out.println("------------------------------------------------------------");
+//     System.out.println("Connected");
+//     System.out.println("------------------------------------------------------------");
 
       Thread.sleep(2000);
 
       for (int i = 0; i < 100; i++) {
         Thread.sleep(500);
 
-        System.out.println("Publishing message: " + content);
+//         System.out.println("Publishing message: " + content);
         MqttMessage message = new MqttMessage(content.getBytes());
         message.setQos(qos);
         sampleClient.publish(topic, message);
-        System.out.println("Message published");
+//         System.out.println("Message published");
 
       }
 
 
       // Disconnecting
-      System.out.println("------------------------------------------------------------");
+//       System.out.println("------------------------------------------------------------");
       sampleClient.disconnect();
-      System.out.println("Disconnected");
+//       System.out.println("Disconnected");
       System.exit(0);
     } catch (MqttException me) {
-      System.out.println("reason " + me.getReasonCode());
-      System.out.println("msg " + me.getMessage());
-      System.out.println("loc " + me.getLocalizedMessage());
-      System.out.println("cause " + me.getCause());
-      System.out.println("excep " + me);
+//       System.out.println("reason " + me.getReasonCode());
+//       System.out.println("msg " + me.getMessage());
+//       System.out.println("loc " + me.getLocalizedMessage());
+//       System.out.println("cause " + me.getCause());
+//       System.out.println("excep " + me);
       me.printStackTrace();
       System.exit(1);
     } catch (InterruptedException i) {
-      System.out.println("time exeption");
+//       System.out.println("time exeption");
       i.printStackTrace();
       System.exit(1);
 
